7b3ba3c942ac1b6977f38cb10f0e92d1051a3f1c
octo-catto <70869143+octo-catto@users.noreply.github.com>
2023-03-13 10:11:04 -0500

    Update programaCuatro.cpp

--- programaCuatro.cpp
+++ programaCuatro.cpp
@@ -8,13 +8,23 @@ int main()
 {
     clock_t T;
     unsigned int i;
-    unsigned long A = 50;
+    unsigned long A = 2000;
+    unsigned long B = 1000;
+    unsigned long C = 100;
+    T = clock();
     for (i = A - 1; i > 0; i--)
     {
         A *= i;
     }
-    cout << numeric_limits<unsigned long long int>::max() << endl;
-    cout << numeric_limits<unsigned long int>::max() << endl;
-    cout << numeric_limits<unsigned int>::max() << endl;
+    for (i = B - 1; i > 0; i--)
+    {
+        B *= i;
+    }
+    for (i = C - 1; i > 0; i--)
+    {
+        C *= i;
+    }
+    T = clock() - T;
+    printf("El programa cuatro se ejecutó en: %f segundos. \n", (float)T/CLOCKS_PER_SEC);
     return 0;
 }

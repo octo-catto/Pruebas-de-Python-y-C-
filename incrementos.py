a1d703587c5c77d4dcc33ad65d1f7d73cea748e0
octo-catto <70869143+octo-catto@users.noreply.github.com>
2023-03-14 09:22:22 -0500

    Update incrementos.py

--- incrementos.py
+++ incrementos.py
@@ -1,10 +1,7 @@
 import time
 inicio = time.time()
-for i in range(1, 10000):
-    A = 0
-for i in range(10000):
+A = 0
+for i in range(1000000000):
     A = A + 1
-    print(A)
-    fin = time.time()
-print(fin-inicio)
-
+fin = time.time()
+print('%.3f' % float(fin-inicio))

f377fbec1e20d4dba1dae80a32b39b1808d53431
octo-catto <70869143+octo-catto@users.noreply.github.com>
2023-03-14 09:18:45 -0500

    Update textoplano.py

--- textoplano.py
+++ textoplano.py
@@ -1,5 +1,7 @@
+import time
+inicio = time.time()
 for i in range(1000):
     with open('archivo.txt', 'a') as f:
         f.write(str(i) + '\n')
-   
-
+fin = time.time()
+print('%.3f' % float(fin-inicio))
